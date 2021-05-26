d9565bef6603839f5ade7739be57c78a97ca779f
Lawliet <edwardmario123@gmail.com>
2021-05-26 08:24:47 -0500

    Convertimos el bot en cogs

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import discord
-from models.basics import token, bot
-import models.help
+from default.basics import token, bot
+import os
 
 @bot.event
 async def on_ready():
@@ -11,5 +11,16 @@ async def on_ready():
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
+
+if __name__ == "__main__":
+    for file in os.listdir("./cogs"):
+        if file.endswith(".py"):
+            extension = file[:-3]
+            try:
+                bot.load_extension(f"cogs.{extension}")
+                print(f"Loaded extension '{extension}'")
+            except Exception as e:
+                exception = f"{type(e).__name__}: {e}"
+                print(f"Failed to load extension {extension}\n{exception}")
 
 bot.run(token)
