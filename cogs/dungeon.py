7dbca8b4ec146a496ed326aa11a31e87fb8e7d75
Lawliet <edwardmario123@gmail.com>
2021-06-04 09:37:55 -0500

    Creamos los stats de cada moustro y la verificacion se esta el jugador en la db para jugar

--- cogs/dungeon.py
+++ cogs/dungeon.py
@@ -14,7 +14,7 @@ class Dungeon(commands.Cog, name = 'dungeon'):
         self.bot = bot
         self.createdungeon.start()
     
-    @tasks.loop(seconds = 15)
+    @tasks.loop(minutes = 15)
     async def createdungeon(self):
         payload = {'rank'       :       numpy.random.choice(dung['rank'], p = [0.33, 0.27, 0.20, 0.14, 0.05, 0.01]),
                    'monster'    :       numpy.random.choice(dung['monster'], p = [0.50, 0.50])}
@@ -23,18 +23,25 @@ class Dungeon(commands.Cog, name = 'dungeon'):
         
     @commands.command(name = 'dungeonlist', aliases = ['dungl'])
     async def dungeonlist(self, ctx):
-        idg = []
-        rank = []
-        for x in db.Dungeons.find():
-            idg.append(str(x['_id']))
-            rank.append(x['rank'])
-        dic = (dict(zip(rank, idg)))
-        data = '\n'.join(":black_small_square: **{}:** `{}`".format(item, value) for item, value in dic.items())
+        users_list = []
+        for user in db.Users.find():
+            users_list.append(int(user['_id']))
         
-        embed = discord.Embed(title = '**Dungeons List**',
-                              color = 0x800080)
-        embed.add_field(name = 'Complete all.', value = data)
-        await ctx.send(embed = embed)
+        if int(ctx.message.author.id) in users_list:
+            idg = []
+            rank = []
+            for x in db.Dungeons.find():
+                idg.append(str(x['_id']))
+                rank.append(x['rank'])
+            dic = (dict(zip(rank, idg)))
+            data = '\n'.join(":black_small_square: **{}:** `{}`".format(item, value) for item, value in dic.items())
+            
+            embed = discord.Embed(title = '**Dungeons List**',
+                                color = 0x800080)
+            embed.add_field(name = 'Complete all.', value = data)
+            await ctx.send(embed = embed)
+        else:
+            await ctx.send('You do not have a registered account, use `start` to play.')
         
 def setup(bot):
     bot.add_cog(Dungeon(bot))
