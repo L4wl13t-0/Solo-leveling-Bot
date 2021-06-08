4d642a7b466cd91000b8942954832be58b5fa909
Lawliet <edwardmario123@gmail.com>
2021-06-08 08:49:05 -0500

    Actualizamos help

--- cogs/help.py
+++ cogs/help.py
@@ -10,7 +10,8 @@ class Help(commands.Cog, name = 'help'):
         embed = discord.Embed(title = "Help menu",
                               description = "Use `;help <command>` for extended information on a command.",
                               color = ctx.author.color)
-        embed.add_field(name = "Statistics", value = "`profile`, `inventory`")
+        embed.add_field(name = "Statistics", value = "`profile`, `inventory`", inline = False)
+        embed.add_field(name = "Dungeons", value = "`dungeonlist`, `dungeonjoin`, `dungeonleave`", inline = False)
         await ctx.send(embed = embed)
       
     @help.command()
@@ -30,6 +31,36 @@ class Help(commands.Cog, name = 'help'):
                               color = ctx.author.color)
         embed.add_field(name = "Use", value = "`;inventory`")
         embed.add_field(name = "Alias", value = "`;i`")
+        embed.set_footer(text = "Wrong command? use; help to see all the commands.")
+        await ctx.send(embed = embed)
+        
+    @help.command()
+    async def dungeonlist(self, ctx):
+        embed = discord.Embed(title = 'Dungeonlist',
+                              description = 'Show all active dungeons.',
+                              color = ctx.author.color)
+        embed.add_field(name = 'Use', value = '`;dungeonlist`')
+        embed.add_field(name = 'Alias', value = '`;dglist`')
+        embed.set_footer(text = "Wrong command? use; help to see all the commands.")
+        await ctx.send(embed = embed)
+        
+    @help.command()
+    async def dungeonjoin(self, ctx):
+        embed = discord.Embed(title = 'Dungeonjoin',
+                              description = 'To enter the queue of a dungeon.',
+                              color = ctx.author.color)
+        embed.add_field(name = 'Use', value = '`;dungeonjoin <id>`')
+        embed.add_field(name = 'Alias', value = '`;dgjoin <id>`')
+        embed.set_footer(text = "Wrong command? use; help to see all the commands.")
+        await ctx.send(embed = embed)
+        
+    @help.command()
+    async def dungeonleave(self, ctx):
+        embed = discord.Embed(title = 'Dungeonleave',
+                              description = 'To leave the queue of a dungeon.',
+                              color = ctx.author.color)
+        embed.add_field(name = 'Use', value = '`;dungeonleave <id>`')
+        embed.add_field(name = 'Alias', value = '`;dgleave <id>`')
         embed.set_footer(text = "Wrong command? use; help to see all the commands.")
         await ctx.send(embed = embed)
 
